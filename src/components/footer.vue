257b12e542dcc508408835c729e1cb08aff76c1a
junghuang <junguanghuang@gmai.com>
2020-11-08 10:58:18 +0800

    basic components

---
+++ src/components/footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    © 2020 <a href="https://github.com/junghuang" target="_blank">junghuang</a>
+  </footer>
+</template>
+
+<style>
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  background: #f8f8f8;
+  text-align: center;
+  font-size: 14px;
+  color: #666;
+  border-top: 1px solid #ddd;
+}
+
+.footer a {
+  color: #666;
+}
+</style>
