125252128bec545a3bf36fc6fb3abca970b74c93
junghuang <junguanghuang@gmai.com>
2020-11-08 11:10:51 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import event from './event'
+import theme from './theme'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    event,
+    theme
   }
 })
